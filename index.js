fe21f5521bc0d1e49ca5c4ceeccabdc746de9022
Julia Shi <julia.m.shi@yale.edu>
2017-04-02 01:31:02 +0000

    Trying to render index.html

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express');
+var path = require('path')
 var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(path.join(__dirname, 'public')));app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+
+app.set('view engine', 'ejs');
+
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.render('index.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
