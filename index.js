228bb329164cabaa160aed4b29de882b86805c50
Julia Shi <julia.m.shi@yale.edu>
2017-04-02 01:05:31 +0000

    testing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
