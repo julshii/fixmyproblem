7384f447133b947a00228f39260b095f5704bd2e
Emily Chen <Emily@Emilys-MBP-5.wireless.yale.internal>
2017-04-06 22:38:50 -0400

    sign up function works and can post problem

--- index.js
+++ index.js
@@ -3,12 +3,47 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const pg = require('pg');
+var bodyParser = require('body-parser');
+var localAuthFactory = require('express-local-auth');
 
+let databaseClient = null;
 
-// Connection doesn't seem to be working
-// const pg = require('pg');
-// const connectionString = "postgres://qhefgqzvsceoir:995051d605051c867da9e8f55829c3baad567dfe3d076178ab133085e5a248a5@ec2-54-163-233-89.compute-1.amazonaws.com:5432/d6fb6km232dln";
-// const client = new pg.Client(connectionString);
+var connectionString = "postgres://qhefgqzvsceoir:995051d605051c867da9e8f55829c3baad567dfe3d076178ab133085e5a248a5@ec2-54-163-233-89.compute-1.amazonaws.com:5432/d6fb6km232dln?ssl=true"
+
+pg.connect(connectionString, function(err, client, done) {
+  console.log('Connected to postgres! Getting schemas...');
+
+    if(err){
+        console.log("Error!", err);
+        return;
+    }
+
+    // Connection to database ok! Now, let's start our server.
+    databaseClient = client;
+    client.query('SELECT email FROM users where id = 1', (err, result) => {
+      console.log('QUERY WAS RUN');
+      for (var i = 0; i < result.rows.length; i++) {
+        console.log(result.rows[i]);
+      }
+    });
+
+});
+
+// var services = {
+//     emailService: myEmailService,
+//     userStore: myUserStore,
+//     passwordResetTokenStore: myPasswordResetTokenStore,
+//     verifyEmailTokenStore: myVerifyEmailTokenStore,
+//     logger: logger
+// };
+
+// var options = {
+//   failedLoginsBeforeLockout: 5,
+// };
+//
+// var localAuth = localAuthFactory(app, services, options);
+
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -28,7 +63,7 @@ app.get('/', function(req, res) {
 // set the login page route
 app.get('/login', function(req, res) {
     //   Currently not working:
-    
+
     //   client.connect(function (err) {
     //   if (err) {
     //       console.log("ERROR");
@@ -36,7 +71,7 @@ app.get('/login', function(req, res) {
     //   }
     //   var info = client.query("SELECT * FROM users");
     //   console.log(info);
-      
+
     //   client.end(function (err) {
     //     if (err) throw err;
     //   });
@@ -50,15 +85,52 @@ app.get('/signup', function(req, res) {
     res.render('signup.html');
 });
 
-// post request not working
-// app.post('/signup', function (req, res) {
-//   res.redirect('/home');
+// POST request
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/signup', function (req, res) {
+  if (req.body.password !== req.body.confirm) {
+    res.redirect('/signup');
+    console.log('Passwords do not match!');
+  }
+  else {
+    const sql = 'INSERT INTO users(email, password) VALUES ($1, $2) RETURNING id'
+    const values = [req.body.email, req.body.password];
+    databaseClient.query(sql, values, function(err, result) {
+      if(err) {
+        console.log('login failure')
+      }
+    res.redirect('/home');
+    });
+  }
+});
+
+
+// app.post('/login', localAuth.login(), function(req, res) {
+//     res.redirect('/home');
 // });
 
 // set the home page route
 app.get('/home', function(req, res) {
     // ejs render automatically looks in the views folder
     res.render('home.html');
+});
+
+app.post('/home', function (req, res) {
+  const sql1 = 'INSERT INTO posts(post, options) VALUES ($1, $2) RETURNING id'
+  const values1 = [req.body.problem, req.body.option1];
+  // const option1 = req.body.option1;
+  // const option2 = req.body.option2;
+  // const option3 = req.body.option3;
+  // const values1 = [req.body.problem, "{$option1, $option2, $option3}"];
+  databaseClient.query(sql1, values1, function(err, result) {
+    if(err) {
+      console.log('post failed')
+    }
+    res.redirect('/signup');
+  });
 });
 
 // set the problem portfolio page route
